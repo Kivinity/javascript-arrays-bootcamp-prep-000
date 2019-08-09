2e8c8da2d98c0d06c2fccb9140c3225f5d136e88
Kathryn Stenberg <kate.stenberg@hotmail.com>
2019-08-09 19:33:13 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "and skittles"]
+   
+  function addElementToBeginningOfArray(Array)
+  {
+    
+    }
+    
+    addElementToBeginningOfArray.push("marsbar")
+    
+  function destructivelyAddElementToBeginningOfArray(Array)
+  {
+
+  }
+  
+  destructivelyAddElementToBeginningOfArray.unshift("marsbar")
